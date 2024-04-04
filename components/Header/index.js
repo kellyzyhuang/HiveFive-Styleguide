98bd8d71864831feb5cf91dd9bdb58944764dc82
Arabella Alegria <a.bella@Arabellas-MacBook-Air.local>
2024-04-04 01:02:54 -0700

    add link to menu list and style link

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,5 @@
 import styles from "./Header.module.css";
-import Link from "react";
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Header(){
@@ -8,10 +8,11 @@ export default function Header(){
             <Image className={styles.logo}/>
             <div className={styles.menuListContainer}>
                 <ul className={styles.menuList}>
-                    <li className={styles.list}>Logo</li>
-                    <li className={styles.list}>Colours</li>
-                    <li className={styles.list}>Typography</li>
-                    <li className={styles.list}>Graphics</li>
+                    <li className={styles.list}><Link href="/">Logo</Link></li>
+                    <li className={styles.list}><Link href="/Colors">Colours</Link></li>
+                    <li className={styles.list}><Link href="/">Typography</Link></li>
+                    <li className={styles.list}><Link href="/Graphics">Graphics</Link></li>
+                    <li className={styles.list}><Link href="/References">References</Link></li>
                 </ul>
             </div>
         </div>
