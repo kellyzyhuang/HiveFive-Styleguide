1f29d71a571b7e905f7f135d9f77980ed7d196f2
Arabella Alegria <a.bella@Arabellas-MacBook-Air.local>
2024-04-04 00:08:08 -0700

    update correct importing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import Header from "/Header";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
