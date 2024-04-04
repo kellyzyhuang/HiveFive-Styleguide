ac2536bdd2140b01f455c15b0269a47e582c0112
Arabella Alegria <a.bella@Arabellas-MacBook-Air.local>
2024-04-04 00:13:22 -0700

    add menu list in header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,6 @@
-import styles from "./Header/module.css";
+import styles from "./Header.module.css";
 import Link from "react";
+import Image from "next/image";
 
 export default function Header(){
     return(
@@ -7,7 +8,9 @@ export default function Header(){
             <Image className={styles.logo}/>
             <div className={styles.menuList}>
                 <ul>
+                    <li>Logo</li>
                     <li>Colours</li>
+                    <li>Typography</li>
                     <li>Graphics</li>
                 </ul>
             </div>
