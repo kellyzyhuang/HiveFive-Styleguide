cf5e035c918e1ead825f757bbab7ed06842478d3
Arabella Alegria <a.bella@Arabellas-MacBook-Air.local>
2024-04-04 02:07:33 -0700

    add basic content of color palette

--- pages/Colors.js
+++ pages/Colors.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import styles from "@/styles/Colors.module.css"
+import Header from "@/components/Header";
 
 export default function Colors() {
   return(
@@ -9,6 +11,22 @@ export default function Colors() {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header/>
+      <main className={`${styles.main}`}>
+        <section>
+          <div className={styles.colorPalette}>
+            <h2>Colour Palette</h2>
+            <div className={styles.white}></div>
+            <p>#FFFFFF</p>
+            <div className={styles.secondary}></div>
+            <p>#FEECAC</p>
+            <div className={styles.accent}></div>
+            <p>#FFC93D</p>
+            <div className={styles.black}></div>
+            <p>#000000</p>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
