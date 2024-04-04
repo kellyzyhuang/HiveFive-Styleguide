266d98fb2c9387990204572d291d5c35046bc313
Arabella Alegria <a.bella@Arabellas-MacBook-Air.local>
2024-04-04 00:33:02 -0700

    align list

--- components/Header/index.js
+++ components/Header/index.js
@@ -6,12 +6,12 @@ export default function Header(){
     return(
         <div className={styles.Header}>
             <Image className={styles.logo}/>
-            <div className={styles.menuList}>
-                <ul>
-                    <li>Logo</li>
-                    <li>Colours</li>
-                    <li>Typography</li>
-                    <li>Graphics</li>
+            <div className={styles.menuListContainer}>
+                <ul className={styles.menuList}>
+                    <li className={styles.list}>Logo</li>
+                    <li className={styles.list}>Colours</li>
+                    <li className={styles.list}>Typography</li>
+                    <li className={styles.list}>Graphics</li>
                 </ul>
             </div>
         </div>
